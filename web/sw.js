e7755320bd067a651dd96f221834f0d9ff604d49
dan <https://github.com/DanilloSB/Projeto-Cordova2.git>
2021-08-07 23:48:16 -0300

    projeto PWA gat and dog

---
+++ web/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('static-v1')
+	    .then(cache => cache.add('/cat.svg'))
+    );
+});
+
+self.addEventListener('activate', e => {});
+
+
+self.addEventListener('fetch', fetchEvent => {
+    console.log("Interceptado evento fecth");
+    console.dir(fecthEvent);
+});
